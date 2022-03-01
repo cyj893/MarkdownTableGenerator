cd3d02b4ba30a81183bdec153a25a43c4f1d1a0c
cyj893 <cyj89317@gmail.com>
2022-03-02 00:49:17 +0900

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:markdown_table_generator/show_res.dart';
-import 'package:markdown_table_generator/table_menu.dart';
 import 'package:markdown_table_generator/width_provider.dart';
 import 'package:provider/provider.dart';
 
-import 'my_div.dart';
-import 'table_helper.dart';
+import 'my_hom_page.dart';
 
 void main() {
   runApp(
@@ -30,50 +27,6 @@ class MyApp extends StatelessWidget {
         canvasColor: Colors.white,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  TableHelper tableHelper = TableHelper();
-
-  @override
-  void initState() {
-    super.initState();
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("마크다운 표 생성기"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const TableMenu(),
-              horizontalDiv(MediaQuery.of(context).size.width),
-              const SizedBox(height: 10,),
-              tableHelper.tableManager!,
-              const SizedBox(height: 10,),
-              const ShowRes(),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
