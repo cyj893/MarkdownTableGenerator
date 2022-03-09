50d53182a5e9cf01b516ba60a21a10e35d041003
cyj893 <cyj89317@gmail.com>
2022-03-09 15:50:47 +0900

    fix: improove condition in mobile

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -13,14 +13,34 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   TableHelper tableHelper = TableHelper();
+
+  var _isKeyboardVisible = false;
 
   @override
   void initState() {
     super.initState();
 
+    WidgetsBinding.instance?.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    final bottomInset = WidgetsBinding.instance!.window.viewInsets.bottom;
+    final newValue = bottomInset > 0.0;
+    if (newValue != _isKeyboardVisible) {
+      setState(() {
+        _isKeyboardVisible = newValue;
+      });
+    }
   }
 
   final ScrollController horizontalScroll = ScrollController();
@@ -28,39 +48,44 @@ class _MyHomePageState extends State<MyHomePage> {
   final double width = 20;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("마크다운 표 생성기", style: TextStyle(fontFamily: "D2Coding"),),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const TableMenu(),
-                    horizontalDiv(MediaQuery.of(context).size.width),
-                    const SizedBox(height: 10,),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: 500,
-                      child: tableHelper.tableManager!,
-                    ),
-                    const SizedBox(height: 10,),
-                    horizontalDiv(MediaQuery.of(context).size.width),
-                    const ShowRes(),
-                    const SizedBox(height: 10,),
-                  ],
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("마크다운 표 생성기", style: TextStyle(fontFamily: "D2Coding"),),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const TableMenu(),
+                      horizontalDiv(MediaQuery.of(context).size.width),
+                      const SizedBox(height: 10,),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 500,
+                        child: tableHelper.tableManager!,
+                      ),
+                      const SizedBox(height: 10,),
+                      horizontalDiv(MediaQuery.of(context).size.width),
+                      const ShowRes(),
+                      const SizedBox(height: 10,),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          const DescriptionContainer(),
-        ],
+            _isKeyboardVisible? const SizedBox.shrink() : const DescriptionContainer(),
+          ],
+        ),
       ),
     );
   }
